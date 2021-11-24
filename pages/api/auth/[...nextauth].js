98402827b3a5f049c3554b78ba3d304d82008757
Kamil Zurek <kamil.zurek@siemens.com>
2021-11-24 09:02:20 +0000

    Auth callbacks, basic sign in / out

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,40 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import CredentialsProvider from 'next-auth/providers/credentials'
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Credentials',
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: 'Email', type: 'text', placeholder: 'john@doe.com' },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials, req) {
+        const res = await fetch('http://localhost:3000/api/login', {
+          method: 'POST',
+          body: JSON.stringify(credentials),
+          headers: { 'Content-Type': 'application/json' }
+        })
+        const user = await res.json()
+        console.log(user)
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          return user
+        }
+        // Return null if user data could not be retrieved
+        return null
+      }
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
     })
-    // ...add more providers here
   ]
 })
--- pages/login.js
+++ pages/login.js
@@ -1,57 +1,28 @@
+import NextLink from 'next/link'
 import { Spinner } from '@chakra-ui/spinner'
-import { useSession, signIn } from 'next-auth/react'
+import { providers, useSession, signIn } from 'next-auth/react'
 import Router from 'next/router'
 import { useEffect, useState } from 'react'
 import Layout from '../components/layouts/article'
-import { Container, Heading, Box, Button } from '@chakra-ui/react'
+import {
+  Container,
+  Heading,
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  FormHelperText,
+  Link,
+  HStack
+} from '@chakra-ui/react'
 import Section from '../components/section'
-
-export async function getStaticProps(context) {
-  const user = process.env.DB_USERNAME ? process.env.DB_USERNAME : ''
-  const pass = process.env.DB_PASSWORD ? process.env.DB_PASSWORD : ''
-
-  return {
-    props: { user, pass }
-  }
-}
 
 export default function Component(props) {
   const { data: session } = useSession()
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
 
   useEffect(() => {
-    const env = process.env.NODE_ENV
-
-    if (env === 'development') {
-      console.log('DEBUG Getting ENV username & pass')
-
-      const { user, pass } = props
-      if (user) {
-        setUsername(user)
-      }
-      if (pass) {
-        setPassword(pass)
-      }
-    }
-  }, [])
-
-  const handleLoginClicked = async () => {
-    const res = await fetch('/api/login')
-
-    if (res.status === 200) {
-      const json = await res.json()
-      if (json.auth === true) {
-        Router.push('/app')
-      }
-    }
-  }
-
-  const handleSignUpClicked = () => {
-    Router.push('/signup')
-  }
-
-  useEffect(() => {
+    console.log(session)
     if (session) {
       Router.push('/app')
     }
@@ -69,44 +40,19 @@ export default function Component(props) {
       >
         <Box>
           <Section>
-            <Heading as="h1" align="center" pb="20px" mt="20px">
+            <Heading as="h1" align="center" mt="20px">
               Login
             </Heading>
-
-            {/* <FormControl id="username">
-              <FormLabel>Username / Email address</FormLabel>
-              <Input
-                value={username}
-                onChange={e => setUsername(e.target.value)}
-                type="username"
-              />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-                type="password"
-              />
-              <FormHelperText>
-                <Link href="/forgotpassword">Forgot password?</Link>
-              </FormHelperText>
-            </FormControl> */}
           </Section>
-          {/* <HStack justify="center">
-            <Button colorScheme="teal" w="100px" onClick={handleLoginClicked}>
-              Login
-            </Button>
-            <Button colorScheme="teal" w="100px" onClick={handleSignUpClicked}>
-              Sign up
-            </Button>
-          </HStack> */}
           <Button
             colorScheme="teal"
-            mt="20px"
             mb="20px"
             w="100%"
-            onClick={signIn}
+            onClick={() =>
+              signIn(providers, {
+                callbackUrl: '/app'
+              })
+            }
           >
             Login using providers
           </Button>
