763f860a5ef226a39a67c4ccd60f9b879f064e20
Kamil Zurek <kamil.zurek@siemens.com>
2021-11-25 14:53:03 +0000

    Updated icons in nav bar

--- components/app/navigation.js
+++ components/app/navigation.js
@@ -1,23 +1,33 @@
-import { VStack, Box, Text, Tooltip } from '@chakra-ui/react'
+import { VStack, Box, Text, Tooltip, Icon } from '@chakra-ui/react'
 import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons'
 import styles from './navigation.module.css'
 import { providers, signOut } from 'next-auth/react'
 import { useState } from 'react'
+import LogoutIcon from '../icons/logout'
+import BookIcon from '../icons/bookOpen'
+import UserCircleIcon from '../icons/userCircle'
 
 const NavItem = props => {
-  const { children, onClick } = props
+  const { children, onClick, icon } = props
 
   return (
     <Box
       display="flex"
       alignItems="center"
       flexDir="row"
-      bgColor="blue"
+      borderTop="1px"
+      borderColor="gray"
       w="full"
       h="50px"
       onClick={onClick}
     >
-      <Box ml="10px" w="20px" h="20px" bgColor="red"></Box>
+      {icon ? (
+        <Icon w="20px" h="20px" ml="10px">
+          {icon}
+        </Icon>
+      ) : (
+        <Box ml="10px" w="20px" h="20px" bgColor="red"></Box>
+      )}
       <Text ml="5px">{children}</Text>
     </Box>
   )
@@ -36,6 +46,7 @@ const Navigation = ({ width, height }) => {
       w={width}
       h={height}
       borderRight="1px"
+      borderColor="gray"
     >
       {isExpanded ? (
         <Tooltip label="Collapse">
@@ -71,9 +82,10 @@ const Navigation = ({ width, height }) => {
         <Box display="flex" w="full" h="full">
           <VStack className={styles.BottomParent} spacing={1}>
             <NavItem>Home</NavItem>
-            <NavItem>Albums</NavItem>
-            <NavItem>Profile</NavItem>
+            <NavItem icon={<BookIcon />}>Albums</NavItem>
+            <NavItem icon={<UserCircleIcon />}>Profile</NavItem>
             <NavItem
+              icon={<LogoutIcon />}
               onClick={() => {
                 signOut({
                   callbackUrl: `/login`
